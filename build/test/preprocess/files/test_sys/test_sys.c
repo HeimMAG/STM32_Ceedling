75061c532cb6facaf1f9f4c51579608a07344f81
Mohamad_Ali_Algbara <mohamad.ali.algbara@heimdalytics.com>
2024-12-18 18:41:27 +0100

    Add Project

---
+++ build/test/preprocess/files/test_sys/test_sys.c
@@ -0,0 +1,16 @@
+// CEEDLING NOTICE: This generated file only to be consumed for test runner creation
+
+#include "build/vendor/unity/src/unity.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_sys_NeedToImplement(void)
+{
+    UnityIgnore( (("Need to Implement sys")), (UNITY_UINT)(16));
+}
